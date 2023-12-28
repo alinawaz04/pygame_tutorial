a8f38e5764ec4553a3cb4626a91953ad505705e1
alinawaz04 <alihnawaz@gmail.com>
2023-12-28 13:46:20 -0600

    player implemented as physics entity!

--- game.py
+++ game.py
@@ -1,58 +1,57 @@
 import pygame
 import sys
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
 
 class Game:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption("ninja game")
+
+        # set resolution of window
         self.screen = pygame.display.set_mode((640, 480))
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
-        self.img.set_colorkey((0, 0, 0))
-
-        self.img_pos = [160, 260]
         self.movement = [False, False]
 
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+        self.assets = {
+            "player": load_image("entities/player.png")
+        }
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
 
     def run(self):
         while True:
             self.screen.fill((14,219,248))
 
-            
-
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
-
+            # updating x value of postion (right and left movement)
+            self.player.update(((self.movement[1] - self.movement[0]), 0))
+            self.player.render(self.screen)
 
             for event in pygame.event.get():
+
+                # quit event
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+                # movement events
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key == pygame.K_s:                        
+                    if event.key == pygame.K_d:                        
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.movement[0] = False
-                    if event.key == pygame.K_s:                        
+                    if event.key == pygame.K_d:                        
                         self.movement[1] = False
                 
-
-
-
             pygame.display.update()
+
+            # set fps to 60
             self.clock.tick(60)
 
 Game().run()
